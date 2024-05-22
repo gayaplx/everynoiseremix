0ef99bbeacf82a4cec47bb9ce3e81fbf9c3e312b
gaya <gustaveploix.pro@proton.me>
2024-05-22 22:30:42 +0200

    styled the search bar and added an animation on the validation (state implemented)

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 import "./searchBar.scss";
 
 export default function SearchBar() {
 
+    const [isValidated, setIsValidated] = useState(false); // state that will check if the form is validated (will have to set to true when we have the results of the API)
+
+    function validate(e) {
+        e.preventDefault();
+
+        setIsValidated(!isValidated);
+    }; // function that will check if we have the results of the API (for the moment we only have the validation of the form);
+
     return (
 
-        <form>
-            <input type="text" id="searchBar"/>
+        <form onSubmit={validate}>
+            <input type="text" className={isValidated ? "validated" : "pending"} id="searchBar"/>
         </form>
 
     )
