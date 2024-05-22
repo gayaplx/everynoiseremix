8e8a9786740893d1ed7de9aeb5285d77e86dc3da
gaya <gustaveploix.pro@proton.me>
2024-05-22 22:21:10 +0200

    created the basic of the search bar

---
+++ src/components/searchBar/searchBar.jsx
@@ -0,0 +1,13 @@
+import "./searchBar.scss";
+
+export default function SearchBar() {
+
+    return (
+
+        <form>
+            <input type="text" id="searchBar"/>
+        </form>
+
+    )
+
+}
