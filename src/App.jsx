26a69297132fe61e00826e2765e4554f3057c54d
gaya <gustaveploix.pro@proton.me>
2024-05-22 22:08:14 +0200

    created an empty boilerplate with only the searchbar component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SearchBar from "./components/searchBar/searchBar";
+
+import "./globalStyles.scss"
+
+export default function App() {
+    return(
+        <section id="body">
+            <SearchBar/>
+        </section>
+    )
+}
