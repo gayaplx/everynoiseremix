ed9090d031ab4a63f14d81f7cb83635880b7f6d9
gustave ploix <gustaveploix.pro@proton.me>
2024-05-27 17:05:12 +0200

    created a function that will get an access token

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,53 @@
-import React, { useState, useTransition } from "react";
-
+import React, { useEffect, useState } from "react";
+import env from "react-dotenv"; 
 // COMPONENTS
 import SearchBar from "./components/searchBar/searchBar";
 import Title from "./components/title/title";
 import Results from "./components/results/results";
-
 // COMPONENTS
+
+// FUNCTIONS
+// import APIFetcher from "./services/APIfetcher";
+// FUNCTIONS
 
 // STYLESHEET
 import "./globalStyles.scss";
 // STYLESHEET
 
+
 export default function App() {
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
   // STATES
   const [isValidated, setIsValidated] = useState(false); // state that will check if the input values are valid (will have to check if the API returns 200);
   const [artistName, setArtistName] = useState(""); // state that stores the name of the artist that we want to find;
+  const [accessToken, setAccessToken] = useState("");
   // STATES
+
+  useEffect(() => {
+    var authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        CLIENT_ID +
+        "&client_secret=" +
+        CLIENT_SECRET,
+    };
+  
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((results) => results.json())
+      .then((data) => {
+      //   console.log(data);
+        setAccessToken(data.access_token)
+      });
+  }, [])
+
+  // useEffect(() => {
+  //   console.log("access token => " + accessToken)
+  // }, [accessToken])
 
   return (
     <section id="mainContainer">
@@ -26,9 +58,11 @@ export default function App() {
           isValidated={isValidated}
           setArtistName={setArtistName}
           artistName={artistName}
+          accessToken={accessToken}
+          setAccessToken={setAccessToken}
         />
       </div>
-      <Results artistName={artistName} isValidated={isValidated} />
+      <Results artistName={artistName} isValidated={isValidated} setAccessToken={setAccessToken} accessToken={accessToken} />
     </section>
   );
 }
