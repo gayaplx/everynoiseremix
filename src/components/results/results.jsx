4e6989127da237406d571f39131408bb67216898
gustave ploix <gustaveploix.pro@proton.me>
2024-05-27 17:46:03 +0200

    displays the different genres

--- src/components/results/results.jsx
+++ src/components/results/results.jsx
@@ -1,3 +1,3 @@
-export default function Results({ artistName, isValidated, accessToken }) {
-  return <h1>{accessToken}</h1>;
+export default function Results({ artistName, isValidated, accessToken, genres }) {
+  return <h1>{genres}</h1>;
 }
