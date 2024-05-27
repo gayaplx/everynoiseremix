ef52057f9c077824bf162b021aff216f1ce501bc
gustave ploix <gustaveploix.pro@proton.me>
2024-05-27 17:06:04 +0200

    cleaned up the searchbar component

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,23 +1,23 @@
 import "./searchBar.scss";
 
-import APIFetcher from "../../services/APIfetcher";
+// import APIFetcher from "../../services/APIfetcher";
 import { useEffect, useState } from "react";
+
+
 
 export default function SearchBar({
   setIsValidated,
   isValidated,
   setArtistName,
   artistName,
+  accessToken,
+  setAccessToken
 }) {
-
-  
 
   function validate(e) {
     e.preventDefault();
 
     setArtistName(e.target[0].value); // will store the name of the artist in a state (App.jsx);
-
-    console.log(artistName)
 
     setIsValidated(true); // sets the validation state to true (App.jsx) => will have to check if the API returns 200;
   } // function that will check if we have the results of the API (for the moment we only have the validation of the form);
