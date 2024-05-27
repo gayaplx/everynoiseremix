744916e7f7a8e6011210934a86ca856c67cb4c7c
gustave ploix <gustaveploix.pro@proton.me>
2024-05-27 19:09:52 +0200

    cleaned up the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
 import React, { useEffect, useState } from "react";
-import env from "react-dotenv"; 
+
 // COMPONENTS
 import SearchBar from "./components/searchBar/searchBar";
 import Title from "./components/title/title";
 import Results from "./components/results/results";
 // COMPONENTS
 
-// FUNCTIONS
-// import APIFetcher from "./services/APIfetcher";
-// FUNCTIONS
-
 // STYLESHEET
 import "./globalStyles.scss";
 // STYLESHEET
 
-
 export default function App() {
-  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID; // calling the spotify API parameters
+  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET; // calling the spotify API parameters
   // STATES
   const [isValidated, setIsValidated] = useState(false); // state that will check if the input values are valid (will have to check if the API returns 200);
   const [artistName, setArtistName] = useState(""); // state that stores the name of the artist that we want to find;
-  const [accessToken, setAccessToken] = useState("");
-  const [genres, setGenres] = useState([])
+  const [accessToken, setAccessToken] = useState(""); // state that store the access token for the spotify API;
+  const [genres, setGenres] = useState([]); // state that stores the genres that is given by the API call;
   // STATES
 
-  useEffect(() => { // will generate an access token for the Spotify API
+  useEffect(() => {
+    // will generate an access token for the Spotify API
     var authParams = {
       method: "POST",
       headers: {
@@ -37,14 +33,13 @@ export default function App() {
         "&client_secret=" +
         CLIENT_SECRET,
     };
-  
+
     fetch("https://accounts.spotify.com/api/token", authParams)
       .then((results) => results.json())
       .then((data) => {
-        setAccessToken(data.access_token) // sets the accessToken state with the result of the API call
+        setAccessToken(data.access_token); // sets the accessToken state with the result of the API call
       });
-  }, [])
-
+  }, []); // runs directly after the launch of the website;
 
   return (
     <section id="mainContainer">
@@ -60,7 +55,7 @@ export default function App() {
           setGenres={setGenres}
         />
       </div>
-      <Results artistName={artistName} isValidated={isValidated} setAccessToken={setAccessToken} accessToken={accessToken} genres={genres} />
+      <Results accessToken={accessToken} genres={genres} />
     </section>
   );
 }
